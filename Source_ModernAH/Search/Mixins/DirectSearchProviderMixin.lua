2a6f9b93c214169c08b0768689e75f113581f1a0
Vadim <vadmikerin@gmail.com>
2026-05-31 15:35:05 +0400

    feat: rewrite Shopping search to use SearchButton+polling
    - DirectSearchProviderMixin: replace SendBrowseQuery with SearchButton:Click()
    - KeywordSearchProviderMixin: same polling approach
    - Poll browseResults + RequestMoreBrowseResults until HasFullBrowseResults
    - Filter system unchanged - results still pass through all filters
    - Fixes shopping lists on Firestorm where SendBrowseQuery is broken

--- Source_ModernAH/Search/Mixins/DirectSearchProviderMixin.lua
+++ Source_ModernAH/Search/Mixins/DirectSearchProviderMixin.lua
@@ -1,16 +1,11 @@
 AuctionatorDirectSearchProviderMixin = CreateFromMixins(AuctionatorMultiSearchMixin, AuctionatorSearchProviderMixin)
 
-local ADVANCED_SEARCH_EVENTS = {
-  "AUCTION_HOUSE_BROWSE_RESULTS_UPDATED",
-  "AUCTION_HOUSE_BROWSE_RESULTS_ADDED",
-  "AUCTION_HOUSE_BROWSE_FAILURE",
-  "GET_ITEM_INFO_RECEIVED",
-  "EXTRA_BROWSE_INFO_RECEIVED",
-}
+-- Firestorm patch: C_AuctionHouse.SendBrowseQuery() doesn't work.
+-- Instead we use SearchButton:Click() + poll browseResults + RequestMoreBrowseResults()
+-- This replaces event-based flow with polling-based flow.
 
-local INTERNAL_SEARCH_EVENTS = {
-  Auctionator.Search.Events.SearchResultsReady
-}
+local POLL_INTERVAL = 0.5  -- seconds between polls
+local POLL_TIMEOUT = 30    -- max seconds to wait for results
 
 local QUALITY_TO_FILTER = {
   [0] = Enum.AuctionHouseFilter.PoorQuality,
@@ -70,20 +65,100 @@ end
 function AuctionatorDirectSearchProviderMixin:GetSearchProvider()
   Auctionator.Debug.Message("AuctionatorDirectSearchProviderMixin:GetSearchProvider()")
 
-  --Run the query, and save extra filter data for processing
   return function(searchTerm)
-    Auctionator.AH.SendBrowseQuery(searchTerm.query)
     self.currentFilter = searchTerm.extraFilters
     self.resultMetadata = searchTerm.resultMetadata
     self.waiting = 0
+    self.pollStartTime = time()
+    self.lastPollCount = 0
+    self.searchComplete = false
+
+    -- Use SearchButton:Click() with the search string
+    local searchString = searchTerm.query.searchString or ""
+    self:FirestormBrowseSearch(searchString)
+  end
+end
+
+function AuctionatorDirectSearchProviderMixin:FirestormBrowseSearch(searchString)
+  Auctionator.Debug.Message("Firestorm Shopping: searching for '" .. searchString .. "'")
+
+  if not AuctionHouseFrame or not AuctionHouseFrame:IsShown() then
+    Auctionator.Debug.Message("Firestorm Shopping: AH not open")
+    self:AddResults({})
+    return
+  end
+
+  local searchBar = AuctionHouseFrame.SearchBar
+  if searchBar and searchBar.SearchBox then
+    searchBar.SearchBox:SetText(searchString)
+  end
+  if searchBar and searchBar.SearchButton then
+    searchBar.SearchButton:Click()
+  end
+
+  -- Start polling for results
+  C_Timer.After(POLL_INTERVAL + 0.5, function() self:PollSearchResults() end)
+end
+
+function AuctionatorDirectSearchProviderMixin:PollSearchResults()
+  if self.searchComplete then
+    return
+  end
+
+  -- Timeout check
+  if time() - self.pollStartTime > POLL_TIMEOUT then
+    Auctionator.Debug.Message("Firestorm Shopping: poll timeout")
+    self.searchComplete = true
+    self:ProcessFirestormResults()
+    return
+  end
+
+  local hasFull = C_AuctionHouse.HasFullBrowseResults()
+  local browseResultsFrame = AuctionHouseFrame and AuctionHouseFrame.BrowseResultsFrame
+  local browseResults = browseResultsFrame and browseResultsFrame.browseResults or {}
+  local currentCount = #browseResults
+
+  Auctionator.Debug.Message("Firestorm Shopping: poll count=" .. currentCount .. " hasFull=" .. tostring(hasFull))
+
+  if hasFull then
+    self.searchComplete = true
+    self:ProcessFirestormResults()
+  else
+    self.lastPollCount = currentCount
+    C_AuctionHouse.RequestMoreBrowseResults()
+    C_Timer.After(POLL_INTERVAL, function() self:PollSearchResults() end)
+  end
+end
+
+function AuctionatorDirectSearchProviderMixin:ProcessFirestormResults()
+  Auctionator.Debug.Message("Firestorm Shopping: processing results")
+
+  local browseResultsFrame = AuctionHouseFrame and AuctionHouseFrame.BrowseResultsFrame
+  local browseResults = browseResultsFrame and browseResultsFrame.browseResults or {}
+
+  Auctionator.Debug.Message("Firestorm Shopping: " .. #browseResults .. " total results to filter")
+
+  -- Process results through the filter system (same as original)
+  self.waiting = self.waiting + #browseResults
+  for index = 1, #browseResults do
+    local resultInfo = browseResults[index]
+    local filterTracker = CreateAndInitFromMixin(
+      Auctionator.Search.Filters.FilterTrackerMixin,
+      resultInfo
+    )
+    resultInfo.purchaseQuantity = self.resultMetadata.quantity
+    local filters = Auctionator.Search.Filters.Create(resultInfo, self.currentFilter, filterTracker)
+    filterTracker:SetWaiting(#filters)
+  end
+
+  if #browseResults == 0 then
+    self:AddResults({})
   end
 end
 
 function AuctionatorDirectSearchProviderMixin:HasCompleteTermResults()
   Auctionator.Debug.Message("AuctionatorDirectSearchProviderMixin:HasCompleteTermResults()")
-
-  --Loaded all the terms from API, and we have filtered every item
-  return Auctionator.AH.HasFullBrowseResults() and self.waiting == 0
+  return self.searchComplete and self.waiting == 0
 end
 
 function AuctionatorDirectSearchProviderMixin:GetCurrentEmptyResult()
@@ -91,50 +166,13 @@ function AuctionatorDirectSearchProviderMixin:GetCurrentEmptyResult()
 end
 
 function AuctionatorDirectSearchProviderMixin:OnSearchEventReceived(eventName, ...)
-  Auctionator.Debug.Message("AuctionatorDirectSearchProviderMixin:OnSearchEventReceived()", eventName, ...)
-
-  if eventName == "AUCTION_HOUSE_BROWSE_RESULTS_UPDATED" then
-    self:ProcessSearchResults(C_AuctionHouse.GetBrowseResults())
-  elseif eventName == "AUCTION_HOUSE_BROWSE_RESULTS_ADDED" then
-    self:ProcessSearchResults(...)
-  elseif eventName == "AUCTION_HOUSE_BROWSE_FAILURE" then
-    AuctionHouseFrame.BrowseResultsFrame.ItemList:SetCustomError(
-      RED_FONT_COLOR:WrapTextInColorCode(ERR_AUCTION_DATABASE_ERROR)
-    )
-  else
+  -- Firestorm: we don't use events for browse results anymore
+  -- But we still need to handle SearchResultsReady from filter system
+  if eventName == "EXTRA_BROWSE_INFO_RECEIVED" or eventName == "GET_ITEM_INFO_RECEIVED" then
     Auctionator.EventBus
       :RegisterSource(self, "AuctionatorDirectSearchProviderMixin")
       :Fire(self, Auctionator.Search.Events.BlizzardInfo, eventName, ...)
       :UnregisterSource(self)
-  end
-end
-
-function AuctionatorDirectSearchProviderMixin:ProcessSearchResults(addedResults)
-  Auctionator.Debug.Message("AuctionatorDirectSearchProviderMixin:ProcessSearchResults()")
-  
-  if not self.registeredForEvents then
-    self.registeredForEvents = true
-    Auctionator.EventBus:Register(self, INTERNAL_SEARCH_EVENTS)
-  end
-
-  if not Auctionator.AH.HasFullBrowseResults() then
-    Auctionator.AH.RequestMoreBrowseResults()
-  end
-
-  self.waiting = self.waiting + #addedResults
-  for index = 1, #addedResults do
-    local filterTracker = CreateAndInitFromMixin(
-      Auctionator.Search.Filters.FilterTrackerMixin,
-      addedResults[index]
-    )
-    addedResults[index].purchaseQuantity = self.resultMetadata.quantity
-    local filters = Auctionator.Search.Filters.Create(addedResults[index], self.currentFilter, filterTracker)
-
-    filterTracker:SetWaiting(#filters)
-  end
-
-  if #addedResults == 0 then
-    self:AddResults({})
   end
 end
 
@@ -143,21 +181,32 @@ function AuctionatorDirectSearchProviderMixin:ReceiveEvent(eventName, results)
     self.waiting = self.waiting - 1
     if self:HasCompleteTermResults() then
       self.registeredForEvents = false
-      Auctionator.EventBus:Unregister(self, INTERNAL_SEARCH_EVENTS)
+      Auctionator.EventBus:Unregister(self, { Auctionator.Search.Events.SearchResultsReady })
     end
     self:AddResults(results)
   end
 end
 
-
 function AuctionatorDirectSearchProviderMixin:RegisterProviderEvents()
-  self:RegisterEvents(ADVANCED_SEARCH_EVENTS)
+  -- Register for filter completion events
+  if not self.registeredForEvents then
+    self.registeredForEvents = true
+    Auctionator.EventBus:Register(self, { Auctionator.Search.Events.SearchResultsReady })
+  end
+  -- Still register these for item info loading
+  self:RegisterEvents({
+    "GET_ITEM_INFO_RECEIVED",
+    "EXTRA_BROWSE_INFO_RECEIVED",
+  })
 end
 
 function AuctionatorDirectSearchProviderMixin:UnregisterProviderEvents()
-  self:UnregisterEvents(ADVANCED_SEARCH_EVENTS)
+  self:UnregisterEvents({
+    "GET_ITEM_INFO_RECEIVED",
+    "EXTRA_BROWSE_INFO_RECEIVED",
+  })
   if self.registeredForEvents then
     self.registeredForEvents = false
-    Auctionator.EventBus:Unregister(self, INTERNAL_SEARCH_EVENTS)
+    Auctionator.EventBus:Unregister(self, { Auctionator.Search.Events.SearchResultsReady })
   end
 end
--- Source_ModernAH/Search/Mixins/KeywordSearchProviderMixin.lua
+++ Source_ModernAH/Search/Mixins/KeywordSearchProviderMixin.lua
@@ -1,57 +1,92 @@
 AuctionatorKeywordSearchProviderMixin = CreateFromMixins(AuctionatorMultiSearchMixin, AuctionatorSearchProviderMixin)
 
-local KEYWORD_SEARCH_EVENTS = {
-  "AUCTION_HOUSE_BROWSE_RESULTS_UPDATED",
-  "AUCTION_HOUSE_BROWSE_RESULTS_ADDED",
-  "AUCTION_HOUSE_BROWSE_FAILURE"
-}
+-- Firestorm patch: C_AuctionHouse.SendBrowseQuery() doesn't work.
+-- Use SearchButton:Click() + poll browseResults instead.
+
+local POLL_INTERVAL = 0.5
+local POLL_TIMEOUT = 30
 
 function AuctionatorKeywordSearchProviderMixin:CreateSearchTerm(term)
   Auctionator.Debug.Message("AuctionatorKeywordSearchProviderMixin:CreateSearchTerm()", term)
 
-  return  {
-    searchString = term,
-    filters = {},
-    itemClassFilters = {},
-    sorts = {}
-  }
+  return term  -- just the search string
 end
 
 function AuctionatorKeywordSearchProviderMixin:GetSearchProvider()
   Auctionator.Debug.Message("AuctionatorKeywordSearchProviderMixin:GetSearchProvider()")
 
-  return C_AuctionHouse.SendBrowseQuery
+  return function(searchString)
+    self.pollStartTime = time()
+    self.searchComplete = false
+
+    self:FirestormBrowseSearch(searchString)
+  end
+end
+
+function AuctionatorKeywordSearchProviderMixin:FirestormBrowseSearch(searchString)
+  Auctionator.Debug.Message("Firestorm Keyword: searching for '" .. tostring(searchString) .. "'")
+
+  if not AuctionHouseFrame or not AuctionHouseFrame:IsShown() then
+    self:AddResults({})
+    return
+  end
+
+  local searchBar = AuctionHouseFrame.SearchBar
+  if searchBar and searchBar.SearchBox then
+    searchBar.SearchBox:SetText(searchString or "")
+  end
+  if searchBar and searchBar.SearchButton then
+    searchBar.SearchButton:Click()
+  end
+
+  C_Timer.After(POLL_INTERVAL + 0.5, function() self:PollSearchResults() end)
+end
+
+function AuctionatorKeywordSearchProviderMixin:PollSearchResults()
+  if self.searchComplete then
+    return
+  end
+
+  if time() - self.pollStartTime > POLL_TIMEOUT then
+    Auctionator.Debug.Message("Firestorm Keyword: poll timeout")
+    self.searchComplete = true
+    self:ProcessFirestormResults()
+    return
+  end
+
+  local hasFull = C_AuctionHouse.HasFullBrowseResults()
+
+  if hasFull then
+    self.searchComplete = true
+    self:ProcessFirestormResults()
+  else
+    C_AuctionHouse.RequestMoreBrowseResults()
+    C_Timer.After(POLL_INTERVAL, function() self:PollSearchResults() end)
+  end
+end
+
+function AuctionatorKeywordSearchProviderMixin:ProcessFirestormResults()
+  local browseResultsFrame = AuctionHouseFrame and AuctionHouseFrame.BrowseResultsFrame
+  local browseResults = browseResultsFrame and browseResultsFrame.browseResults or {}
+
+  Auctionator.Debug.Message("Firestorm Keyword: " .. #browseResults .. " results")
+
+  self:AddResults(browseResults)
 end
 
 function AuctionatorKeywordSearchProviderMixin:HasCompleteTermResults()
   Auctionator.Debug.Message("AuctionatorKeywordSearchProviderMixin:HasCompleteTermResults()")
-  return C_AuctionHouse.HasFullBrowseResults()
+  return self.searchComplete
 end
 
 function AuctionatorKeywordSearchProviderMixin:OnSearchEventReceived(eventName, ...)
-  Auctionator.Debug.Message("AuctionatorKeywordSearchProviderMixin:OnSearchEventReceived()", eventName, ...)
-
-  if eventName == "AUCTION_HOUSE_BROWSE_RESULTS_UPDATED" then
-    self:ProcessSearchResults(C_AuctionHouse.GetBrowseResults())
-  elseif eventName == "AUCTION_HOUSE_BROWSE_RESULTS_ADDED" then
-    self:ProcessSearchResults(...)
-  elseif eventName == "AUCTION_HOUSE_BROWSE_FAILURE" then
-    AuctionHouseFrame.BrowseResultsFrame.ItemList:SetCustomError(
-      RED_FONT_COLOR:WrapTextInColorCode(ERR_AUCTION_DATABASE_ERROR)
-    )
-  end
-end
-
-function AuctionatorKeywordSearchProviderMixin:ProcessSearchResults(addedResults)
-  Auctionator.Debug.Message("AuctionatorKeywordSearchProviderMixin:ProcessSearchResults()")
-
-  self:AddResults(addedResults)
+  -- No-op on Firestorm, we use polling
 end
 
 function AuctionatorKeywordSearchProviderMixin:RegisterProviderEvents()
-  self:RegisterEvents(KEYWORD_SEARCH_EVENTS)
+  -- No events needed for polling approach
 end
 
 function AuctionatorKeywordSearchProviderMixin:UnregisterProviderEvents()
-  self:UnregisterEvents(KEYWORD_SEARCH_EVENTS)
+  -- No events to unregister
 end
